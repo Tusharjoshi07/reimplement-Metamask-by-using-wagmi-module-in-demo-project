50937454d8061b46b0898c5e943855f6ee832d3a
Tushar Joshi <rn.tusharjoshi007@gmail.com>
2024-08-23 14:59:32 +0530

    third

--- src/components/WalletConnector.js
+++ src/components/WalletConnector.js
@@ -1,67 +1,61 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback } from 'react';
 import { useWeb3React, UnsupportedChainIdError } from '@web3-react/core';
-import { InjectedConnector, NoEthereumProviderError,
-  UserRejectedRequestError as UserRejectedRequestErrorInjected } from '@web3-react/injected-connector';
+import { InjectedConnector, NoEthereumProviderError, UserRejectedRequestError as UserRejectedRequestErrorInjected } from '@web3-react/injected-connector';
 import { UserRejectedRequestError as UserRejectedRequestErrorWalletConnect, WalletConnectConnector } from '@web3-react/walletconnect-connector';
-import { BscConnector,  NoBscProviderError } from '@binance-chain/bsc-connector';
-import { ethers } from "ethers";
-import Web3 from "web3";
+import { BscConnector, NoBscProviderError } from '@binance-chain/bsc-connector';
+import { ethers } from 'ethers';
+import Web3 from 'web3';
+import { useAccount, useConnect, useDisconnect, useNetwork, useSwitchNetwork } from 'wagmi';
 
-let netid = 0;//0 ropsten, 1 bsc
+let netid = 0; // 0 for Ropsten, 1 for BSC
 let provider = null;
 let walletconnect, injected, bsc;
 
 const netlist = [
   {
-    chaind : 3,
-    rpcurl : "https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
-    blockurl : "https://ropsten.etherscan.io/",
-    chainname : "Ropsten Test Network",
-    chainnetname : "RPS",
-    chainsymbol : "ETH",
-    chaindecimals : 18
+    chaind: 3,
+    rpcurl: 'https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+    blockurl: 'https://ropsten.etherscan.io/',
+    chainname: 'Ropsten Test Network',
+    chainnetname: 'RPS',
+    chainsymbol: 'ETH',
+    chaindecimals: 18,
   },
   {
-    chaind : 97,
-    rpcurl : "https://data-seed-prebsc-1-s1.binance.org:8545",
-    blockurl : "https://testnet.bscscan.com/",
-    chainname : "Bnb Chain Test net",
-    chainnetname : "RPS",
-    chainsymbol : "BNB",
-    chaindecimals : 18
+    chaind: 97,
+    rpcurl: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+    blockurl: 'https://testnet.bscscan.com/',
+    chainname: 'Bnb Chain Test net',
+    chainnetname: 'RPS',
+    chainsymbol: 'BNB',
+    chaindecimals: 18,
   },
-]
+];
 
 const defaultRopstenconfig = {
   testing: false,
   autoGasMultiplier: 1.5,
   defaultConfirmations: 1,
-  defaultGas: "6000000",
-  defaultGasPrice: "1000000000000",
-  nodetimeout:10000,
+  defaultGas: '6000000',
+  defaultGasPrice: '1000000000000',
+  nodetimeout: 10000,
 };
 
 function web3ProviderFrom(endpoint, config) {
-    const ethConfig = Object.assign(defaultRopstenconfig, config || {});
+  const ethConfig = Object.assign(defaultRopstenconfig, config || {});
   
-    const providerClass = endpoint.includes("wss")
-      ? Web3.providers.WebsocketProvider
-      : Web3.providers.HttpProvider;
+  const providerClass = endpoint.includes('wss')
+    ? Web3.providers.WebsocketProvider
+    : Web3.providers.HttpProvider;
 
-    // console.log(ethConfig)
-    // console.log(providerClass)  
-
-    return new providerClass(endpoint, {
-      timeout: ethConfig.nodetimeout,
-    });
-  }
+  return new providerClass(endpoint, {
+    timeout: ethConfig.nodetimeout,
+  });
+}
 
 export function getDefaultProvider() {
   if (!provider) {
-    provider = new ethers.providers.Web3Provider(
-      web3ProviderFrom(netlist[netid].rpcurl),
-      netlist[netid].chaind
-    );
+    provider = new ethers.providers.JsonRpcProvider(netlist[netid].rpcurl);
   }
 
   return provider;
@@ -71,14 +65,10 @@ export function setNet(id) {
   netid = id;
 
   walletconnect = new WalletConnectConnector({
-    rpc: { [netlist[netid].chaind]: netlist[netid].rpcurl},
-    // http: { [netlist[netid].chaind]: netlist[netid].rpcurl },
-    // infuraId:
+    rpc: { [netlist[netid].chaind]: netlist[netid].rpcurl },
     qrcode: true,
     pollingInterval: 12000,
   });
-
-  // console.log("wallet connect", walletconnect);
 
   injected = new InjectedConnector({
     supportedChainIds: [netlist[netid].chaind],
@@ -89,137 +79,122 @@ export function setNet(id) {
   });
 }
 
-export function useWalletConnector () {
-
-  const { activate, deactivate} = useWeb3React();
+export function useWalletConnector() {
+  const { activate, deactivate } = useWeb3React();
+  const { connect, connectors } = useConnect();
+  const { disconnect } = useDisconnect();
+  const { chain } = useAccount();
+  const { switchNetwork } = useSwitchNetwork();
+  const { chain: activeChain } = useNetwork();
   const [provider, setProvider] = useState({});
 
   const setupNetwork = async () => {
-    // console.log("setup")
-    const provider = window.ethereum
-    // console.log(provider)
-    if (provider) {
+    const ethereum = window.ethereum;
+    if (ethereum) {
       try {
-        await provider.request({
+        await ethereum.request({
           method: 'wallet_switchEthereumChain',
           params: [
             {
               chainId: `0x${netlist[netid].chaind.toString(16)}`,
-              // chainName: netlist[netid].chainname,
-              // nativeCurrency: {
-              //   name: netlist[netid].chainnetname,
-              //   symbol: netlist[netid].symbol,
-              //   decimals: netlist[netid].decimals,
-              // },
-              // rpcUrls: [`${netlist[netid].rpcurl}`],
-              // blockExplorerUrls: [`${netlist[netid].blockurl}`],
             },
           ],
-        })
-
-        setProvider(provider);
-        return true
+        });
+        
+        return true;
       } catch (error) {
-        return false
+        return false;
       }
     } else {
-      console.error("Can't setup the Default Network network on metamask because window.ethereum is undefined")
-      return false
+      console.error("Can't setup the Default Network network on metamask because window.ethereum is undefined");
+      return false;
     }
-  }
+  };
 
   const loginMetamask = async () => {
-    // console.log("metamask")
-    // console.log(account)
-    // console.log(injected)
-    loginWallet(injected)
-    // loginWallet(injected)
-  }
+    const metamaskConnector = connectors.find((connector) => connector.name === 'MetaMask');
+    if (metamaskConnector) {
+      connect({ connector: metamaskConnector });
+      if (chain?.id !== netlist[netid].chaind) {
+        const hasSetup = await setupNetwork();
+        if (hasSetup) {
+          connect({ connector: metamaskConnector });
+        }
+      }
+    } else {
+      alert('MetaMask connector not found');
+    }
+  };
 
   const loginWalletConnect = async () => {
-    //   console.log("walletconnector")
-    loginWallet(walletconnect)
-  }
+    loginWallet(walletconnect);
+  };
 
   const loginBSC = async () => {
-    loginWallet(bsc)
-  }
+    loginWallet(bsc);
+  };
 
-  const loginWallet = useCallback( (connector) => {
-    // console.log("login wallet")
-    // console.log("connector", connector)
-
+  const loginWallet = useCallback((connector) => {
     if (connector) {
-        // console.log("activate")
-      activate(connector, async (error) => {       
+      activate(connector, async (error) => {
         if (error instanceof UnsupportedChainIdError) {
-          const hasSetup = await setupNetwork()
+          const hasSetup = await setupNetwork();
           if (hasSetup) {
-            // console.log(hasSetup)
-            // console.log("setup error")
-            activate(connector)
+            activate(connector);
           }
         } else {
-          // window.localStorage.removeItem(connectorLocalStorageKey)
           if (error instanceof NoEthereumProviderError || error instanceof NoBscProviderError) {
-            alert("Network Provide Error")
+            alert('Network Provide Error');
           } else if (
             error instanceof UserRejectedRequestErrorInjected ||
             error instanceof UserRejectedRequestErrorWalletConnect
           ) {
-            // if (connector instanceof WalletConnectConnector) {
-            //   const walletConnector = connector as WalletConnectConnector
-            //   walletConnector.walletConnectProvider = null
-            // }
-            alert('Authorization Error' + 'Please authorize to access your account')
+            alert('Authorization Error' + 'Please authorize to access your account');
           } else {
-            alert(error.name + error.message)
+            alert(error.name + error.message);
           }
         }
-      })
+      });
     } else {
-      alert('Unable to find connector' + 'The connector config is wrong')
+      alert('Unable to find connector' + 'The connector config is wrong');
     }
 
-    // console.log("active connector", provider);
-
     setProvider(connector);
-  })
+  }, [activate]);
 
   const logoutWalletConnector = () => {
     deactivate(provider, async (error) => {
-        console.log(error)
-        return false
-      });
-      return true
-  }
+      console.log(error);
+      return false;
+    });
+    return true;
+  };
 
   const logoutMetamask = () => {
-    deactivate(injected, async (error) => {
-      console.log(error)
-      return false
-    });
-    return true
-  }
+    disconnect();
+    return true;
+  };
 
   const logoutWalletConnect = () => {
     deactivate(walletconnect, async (error) => {
-      console.log(error)
-      return false
+      console.log(error);
+      return false;
     });
-    return true
-  }
+    return true;
+  };
 
   const logoutBSC = () => {
     deactivate(bsc, async (error) => {
-    //   console.log(error)
-      return false
+      return false;
     });
-    return true
-  }
+    return true;
+  };
 
-  return {loginMetamask, 
-    loginWalletConnect, 
-    loginBSC, 
-    logoutWalletConnector}
+  return {
+    loginMetamask,
+    loginWalletConnect,
+    loginBSC,
+    logoutWalletConnector,
+    logoutMetamask,
+  };
 }
